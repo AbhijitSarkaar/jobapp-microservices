6285d51223f7bab4e2c9f909653a30c1cd9d02cf
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2026-04-28 00:44:31 +0530

    review microservice - endpoints

--- review/src/main/java/com/microservice/review/exception/GlobalExceptionHandler.java
+++ review/src/main/java/com/microservice/review/exception/GlobalExceptionHandler.java
@@ -1,7 +1,42 @@
 package com.microservice.review.exception;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<APIResponse> customNoResourceFoundExceptionHandler(NoResourceFoundException e) {
+        return new ResponseEntity<>(new APIResponse(e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> customMethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        Map<String, String> map = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(item -> {
+            map.put(item.getField(), item.getDefaultMessage());
+        });
+
+        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<APIResponse> customRuntimeExceptionHandler(RuntimeException e) {
+        return new ResponseEntity<>(new APIResponse(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(CustomResourceNotFoundException.class)
+    public ResponseEntity<APIResponse> customResourceNotFoundExceptionHandler(CustomResourceNotFoundException e) {
+        return new ResponseEntity<>(new APIResponse(e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
 }
--- review/src/main/java/com/microservice/review/model/Review.java
+++ review/src/main/java/com/microservice/review/model/Review.java
@@ -1,11 +1,36 @@
 package com.microservice.review.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
 @Table(name = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull
+    @Size(min = 2, message = "Title needs to be at least 2 characters")
+    private String title;
+
+    @NotNull
+    @Size(min = 5, message = "Description needs to be at least 5 characters")
+    private String description;
+
+    @NotNull
+    @Min(value = 1, message = "review needs to be more than or equal to 1")
+    @Max(value = 5, message = "review needs to be less than or equal to 5")
+    private Integer rating;
+
 }
