c77aaeac95d177b5338f1c0b408221d8b2f3b638
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2026-04-30 00:05:15 +0530

    refactor

--- job/src/main/java/com/microservices/job/config/AppConfig.java
+++ job/src/main/java/com/microservices/job/config/AppConfig.java
@@ -3,11 +3,8 @@ package com.microservices.job.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
@@ -17,12 +14,6 @@ public class AppConfig {
          ModelMapper modelMapper = new ModelMapper();
          modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
          return modelMapper;
-    }
-
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 
 }
--- review/src/main/java/com/microservice/review/config/AppConfig.java
+++ review/src/main/java/com/microservice/review/config/AppConfig.java
@@ -2,10 +2,8 @@ package com.microservice.review.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
@@ -15,11 +13,5 @@ public class AppConfig {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
-    }
-
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
