bc8e15bfb8f8d361f5beb6cdfe9d820836f08b39
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2026-05-02 17:35:14 +0530

    dockerized microservices, gateway, eureka server, db

--- job/src/main/java/com/microservices/job/clients/CompanyClient.java
+++ job/src/main/java/com/microservices/job/clients/CompanyClient.java
@@ -5,7 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "COMPANY-SERVICE")
+@FeignClient(
+        name = "COMPANY-SERVICE",
+        url = "${company-service.url}"
+)
 public interface CompanyClient {
     @GetMapping("/api/companies/{companyId}")
     Company getCompany(@PathVariable("companyId") Long companyId);
